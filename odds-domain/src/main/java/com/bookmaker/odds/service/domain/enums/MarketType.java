88012bc3baac9e5eb9ec95f1a6ba34cd070beb05
AltonChan <>
2024-06-14 16:20:46 +0800

    remove redundant import

--- odds-domain/src/main/java/com/bookmaker/odds/service/domain/enums/MarketType.java
+++ odds-domain/src/main/java/com/bookmaker/odds/service/domain/enums/MarketType.java
@@ -1,9 +1,5 @@
 package com.bookmaker.odds.service.domain.enums;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 public enum MarketType {
     TWO_WAY,
     THREE_WAY,
